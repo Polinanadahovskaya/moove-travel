1c83e852ab10eb6441855b6ac6914fc0390a0331
Polina <polinanadahovskaya@gmail.com>
2025-07-23 09:30:10 +0300

    fix

--- mooveTravel/.nuxt/dist/server/client.manifest.mjs
+++ mooveTravel/.nuxt/dist/server/client.manifest.mjs
@@ -1,1 +1,741 @@
-export { default } from "file:///Users/polinanadahovskaya/Desktop/moove-travel/mooveTravel/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
+export default {
+  "_BJio2VSB.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BJio2VSB.js",
+    "name": "articles",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_Dq7h7Pqt.js"
+    ]
+  },
+  "_BOAzHGzj.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BOAzHGzj.js",
+    "name": "index"
+  },
+  "_BaPug2Hd.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BaPug2Hd.js",
+    "name": "travelGuides",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_Dq7h7Pqt.js"
+    ]
+  },
+  "_BjpPD01T.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BjpPD01T.js",
+    "name": "countries",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_Dq7h7Pqt.js"
+    ]
+  },
+  "_C0dnICKA.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "C0dnICKA.js",
+    "name": "asyncData",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "_Ciuh44V7.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "Ciuh44V7.js",
+    "name": "tourOrder",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_Dq7h7Pqt.js"
+    ]
+  },
+  "_Dq7h7Pqt.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "Dq7h7Pqt.js",
+    "name": "index"
+  },
+  "_DrY3dEhF.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DrY3dEhF.js",
+    "name": "pages",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_Dq7h7Pqt.js"
+    ]
+  },
+  "components/bestVariant.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BpiGpZ2m.js",
+    "name": "bestVariant",
+    "src": "components/bestVariant.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": []
+  },
+  "bestVariant.DL8EWxnK.css": {
+    "file": "bestVariant.DL8EWxnK.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "components/gidCoutryTab.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BQsXJLDz.js",
+    "name": "gidCoutryTab",
+    "src": "components/gidCoutryTab.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": []
+  },
+  "gidCoutryTab.enTxc7SA.css": {
+    "file": "gidCoutryTab.enTxc7SA.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "components/popupApplication.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DLv-36sc.js",
+    "name": "popupApplication",
+    "src": "components/popupApplication.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_Ciuh44V7.js",
+      "_BOAzHGzj.js",
+      "components/popupThankYou.vue",
+      "_Dq7h7Pqt.js"
+    ],
+    "css": [],
+    "assets": [
+      "popupBack.qzpijfNF.png",
+      "mainPopup.x6_XjZtw.png"
+    ]
+  },
+  "popupApplication.BXns3pbc.css": {
+    "file": "popupApplication.BXns3pbc.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "popupBack.qzpijfNF.png": {
+    "file": "popupBack.qzpijfNF.png",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png"
+  },
+  "mainPopup.x6_XjZtw.png": {
+    "file": "mainPopup.x6_XjZtw.png",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png"
+  },
+  "components/popupArticle.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "D83Ldr59.js",
+    "name": "popupArticle",
+    "src": "components/popupArticle.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_BOAzHGzj.js",
+      "components/popupThankYou.vue",
+      "_Ciuh44V7.js",
+      "_Dq7h7Pqt.js"
+    ],
+    "css": []
+  },
+  "popupArticle.Csbxlb5W.css": {
+    "file": "popupArticle.Csbxlb5W.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "components/popupBeforePay.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "C9r-hHW5.js",
+    "name": "popupBeforePay",
+    "src": "components/popupBeforePay.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_BOAzHGzj.js",
+      "_Dq7h7Pqt.js"
+    ],
+    "css": []
+  },
+  "popupBeforePay.C8oozuOM.css": {
+    "file": "popupBeforePay.C8oozuOM.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "components/popupThankYou.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BwdTGMuz.js",
+    "name": "popupThankYou",
+    "src": "components/popupThankYou.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": []
+  },
+  "popupThankYou.BsUB1ScI.css": {
+    "file": "popupThankYou.BsUB1ScI.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "components/popupTravelGid.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "CDiO4C4i.js",
+    "name": "popupTravelGid",
+    "src": "components/popupTravelGid.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_BOAzHGzj.js",
+      "components/popupThankYou.vue",
+      "_Ciuh44V7.js",
+      "_Dq7h7Pqt.js"
+    ],
+    "css": []
+  },
+  "popupTravelGid.Ci0Y72in.css": {
+    "file": "popupTravelGid.Ci0Y72in.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "components/teamTab.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "mUqBIFHg.js",
+    "name": "teamTab",
+    "src": "components/teamTab.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": []
+  },
+  "teamTab.DFGyu0SL.css": {
+    "file": "teamTab.DFGyu0SL.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "components/tubArticle.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "C2VLEUDV.js",
+    "name": "tubArticle",
+    "src": "components/tubArticle.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": []
+  },
+  "tubArticle.CQPOrD7P.css": {
+    "file": "tubArticle.CQPOrD7P.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "components/tubCountry.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "SFyL28U5.js",
+    "name": "tubCountry",
+    "src": "components/tubCountry.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": []
+  },
+  "tubCountry.B4wmeAL9.css": {
+    "file": "tubCountry.B4wmeAL9.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "components/tubOffice.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BplMSTKG.js",
+    "name": "tubOffice",
+    "src": "components/tubOffice.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": []
+  },
+  "tubOffice.CufytJJR.css": {
+    "file": "tubOffice.CufytJJR.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DHIVwR32.js",
+    "name": "entry",
+    "src": "node_modules/nuxt/dist/app/entry.js",
+    "isEntry": true,
+    "dynamicImports": [],
+    "css": [
+      "entry.ByFoHDl4.css"
+    ],
+    "assets": [
+      "Plane.CR3c6CTm.svg",
+      "mooveTravel.DgX3fYJE.svg",
+      "samolet.DNgSSQwH.svg"
+    ],
+    "_globalCSS": true
+  },
+  "entry.ByFoHDl4.css": {
+    "file": "entry.ByFoHDl4.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "Plane.CR3c6CTm.svg": {
+    "file": "Plane.CR3c6CTm.svg",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/svg+xml"
+  },
+  "mooveTravel.DgX3fYJE.svg": {
+    "file": "mooveTravel.DgX3fYJE.svg",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/svg+xml"
+  },
+  "samolet.DNgSSQwH.svg": {
+    "file": "samolet.DNgSSQwH.svg",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/svg+xml"
+  },
+  "pages/Travel-gids.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DqjVJAh2.js",
+    "name": "Travel-gids",
+    "src": "pages/Travel-gids.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "components/bestVariant.vue",
+      "components/tubCountry.vue",
+      "components/popupTravelGid.vue",
+      "components/popupApplication.vue",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_DrY3dEhF.js",
+      "_BjpPD01T.js",
+      "_C0dnICKA.js",
+      "_BOAzHGzj.js",
+      "components/popupThankYou.vue",
+      "_Ciuh44V7.js",
+      "_Dq7h7Pqt.js"
+    ],
+    "css": []
+  },
+  "Travel-gids.DiNcXHAg.css": {
+    "file": "Travel-gids.DiNcXHAg.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/about.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "C6Vqfg9M.js",
+    "name": "about",
+    "src": "pages/about.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "components/popupApplication.vue",
+      "components/teamTab.vue",
+      "_DrY3dEhF.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_C0dnICKA.js",
+      "_Ciuh44V7.js",
+      "_Dq7h7Pqt.js",
+      "_BOAzHGzj.js",
+      "components/popupThankYou.vue"
+    ],
+    "css": [],
+    "assets": [
+      "fastOpen.CZJfn7Pg.png"
+    ]
+  },
+  "about.GeR3QDJ3.css": {
+    "file": "about.GeR3QDJ3.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "fastOpen.CZJfn7Pg.png": {
+    "file": "fastOpen.CZJfn7Pg.png",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png"
+  },
+  "pages/article/[link].vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "hqGdi5Ck.js",
+    "name": "_link_",
+    "src": "pages/article/[link].vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "components/popupArticle.vue",
+      "components/popupApplication.vue",
+      "_BJio2VSB.js",
+      "_BOAzHGzj.js",
+      "components/popupThankYou.vue",
+      "_Ciuh44V7.js",
+      "_Dq7h7Pqt.js"
+    ],
+    "css": []
+  },
+  "_link_.D_b4b9hv.css": {
+    "file": "_link_.D_b4b9hv.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/blog.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "D8Y9FxU8.js",
+    "name": "blog",
+    "src": "pages/blog.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "components/popupApplication.vue",
+      "node_modules/nuxt/dist/app/entry.js",
+      "components/tubCountry.vue",
+      "_BjpPD01T.js",
+      "_DrY3dEhF.js",
+      "_BJio2VSB.js",
+      "_Dq7h7Pqt.js",
+      "components/tubArticle.vue",
+      "_C0dnICKA.js",
+      "_Ciuh44V7.js",
+      "_BOAzHGzj.js",
+      "components/popupThankYou.vue"
+    ],
+    "css": []
+  },
+  "blog.Dkz3Rbyn.css": {
+    "file": "blog.Dkz3Rbyn.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/country-guide/[link].vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "CSFLhHds.js",
+    "name": "_link_",
+    "src": "pages/country-guide/[link].vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "components/gidCoutryTab.vue",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_C0dnICKA.js",
+      "_BaPug2Hd.js",
+      "_BjpPD01T.js",
+      "_Dq7h7Pqt.js"
+    ],
+    "css": []
+  },
+  "_link_.DTdedPz2.css": {
+    "file": "_link_.DTdedPz2.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/country/[link].vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DBS_L29t.js",
+    "name": "_link_",
+    "src": "pages/country/[link].vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "components/tubArticle.vue",
+      "_BJio2VSB.js",
+      "_BjpPD01T.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_C0dnICKA.js",
+      "_Dq7h7Pqt.js"
+    ],
+    "css": [],
+    "assets": [
+      "countryArticle.Bc6rpQ4v.jpg"
+    ]
+  },
+  "_link_.DcHjsZJO.css": {
+    "file": "_link_.DcHjsZJO.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "countryArticle.Bc6rpQ4v.jpg": {
+    "file": "countryArticle.Bc6rpQ4v.jpg",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/jpeg"
+  },
+  "pages/guide/[link].vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BSmqjKau.js",
+    "name": "_link_",
+    "src": "pages/guide/[link].vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "components/popupBeforePay.vue",
+      "_BaPug2Hd.js",
+      "_C0dnICKA.js",
+      "_BOAzHGzj.js",
+      "_Dq7h7Pqt.js"
+    ],
+    "css": []
+  },
+  "_link_.BUpJMCmY.css": {
+    "file": "_link_.BUpJMCmY.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DxTBhok_.js",
+    "name": "index",
+    "src": "pages/index.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "components/popupApplication.vue",
+      "_BJio2VSB.js",
+      "_DrY3dEhF.js",
+      "_C0dnICKA.js",
+      "_Ciuh44V7.js",
+      "_Dq7h7Pqt.js",
+      "_BOAzHGzj.js",
+      "components/popupThankYou.vue"
+    ],
+    "css": [],
+    "assets": [
+      "planeStartPage.cpqDyYuo.svg"
+    ]
+  },
+  "index.DilfP8UH.css": {
+    "file": "index.DilfP8UH.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "planeStartPage.cpqDyYuo.svg": {
+    "file": "planeStartPage.cpqDyYuo.svg",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/svg+xml"
+  },
+  "pages/loyarInformation.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "C9UbwmuJ.js",
+    "name": "loyarInformation",
+    "src": "pages/loyarInformation.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": []
+  },
+  "loyarInformation.DMoC7m-w.css": {
+    "file": "loyarInformation.DMoC7m-w.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/payment-callback.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BiZBVJgv.js",
+    "name": "payment-callback",
+    "src": "pages/payment-callback.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_Dq7h7Pqt.js"
+    ],
+    "css": []
+  },
+  "payment-callback.DqR8DEs3.css": {
+    "file": "payment-callback.DqR8DEs3.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "src/assets/images/Plane.svg": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/svg+xml",
+    "file": "Plane.CR3c6CTm.svg",
+    "src": "src/assets/images/Plane.svg"
+  },
+  "src/assets/images/andrew.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "andrew.CFzbSFBa.png",
+    "src": "src/assets/images/andrew.png"
+  },
+  "src/assets/images/countryArticle.jpg": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/jpeg",
+    "file": "countryArticle.Bc6rpQ4v.jpg",
+    "src": "src/assets/images/countryArticle.jpg"
+  },
+  "src/assets/images/fastOpen.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "fastOpen.CZJfn7Pg.png",
+    "src": "src/assets/images/fastOpen.png"
+  },
+  "src/assets/images/mainPopup.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "mainPopup.x6_XjZtw.png",
+    "src": "src/assets/images/mainPopup.png"
+  },
+  "src/assets/images/mooveTravel.svg": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/svg+xml",
+    "file": "mooveTravel.DgX3fYJE.svg",
+    "src": "src/assets/images/mooveTravel.svg"
+  },
+  "src/assets/images/planeStartPage.svg": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/svg+xml",
+    "file": "planeStartPage.cpqDyYuo.svg",
+    "src": "src/assets/images/planeStartPage.svg"
+  },
+  "src/assets/images/popupBack.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "popupBack.qzpijfNF.png",
+    "src": "src/assets/images/popupBack.png"
+  },
+  "src/assets/images/samolet.svg": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/svg+xml",
+    "file": "samolet.DNgSSQwH.svg",
+    "src": "src/assets/images/samolet.svg"
+  },
+  "src/assets/images/tanya.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "tanya.Cc9WdXML.png",
+    "src": "src/assets/images/tanya.png"
+  }
+}
--- mooveTravel/.nuxt/types/build.d.ts
+++ mooveTravel/.nuxt/types/build.d.ts
@@ -12,8 +12,6 @@ declare module "#build/paths.mjs";
 declare module "#build/root-component.mjs";
 declare module "#build/plugins.server.mjs";
 declare module "#build/test-component-wrapper.mjs";
-declare module "#build/devtools/settings.mjs";
-declare module "#build/runtime.vue-devtools-client.Q9Vj0BYrmCLHuBCmFWx1Yb15NI9odni_ge3_J5rSCtQ.js";
 declare module "#build/routes.mjs";
 declare module "#build/pages.mjs";
 declare module "#build/router.options.mjs";
