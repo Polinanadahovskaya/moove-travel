82bfa94dd80e3ed254ca119d4e9462703a08e0de
Polina <polinanadahovskaya@gmail.com>
2025-09-01 15:54:56 +0300

    fix

--- mooveTravel/pages/loyarInformation.vue
+++ mooveTravel/pages/loyarInformation.vue
@@ -2,8 +2,10 @@
   <div>
     <div class="back-country">
       <div class="header-content">
-        <h1>{{ loyarPage?.title.toUpperCase() }}</h1>
-        <button class="back-btn" @click="router.back()">← Назад</button>
+        <div>
+          <h1>{{ loyarPage?.title.toUpperCase() }}</h1>
+          <button class="back-btn" @click="router.back()">← Назад</button>
+        </div>
       </div>
     </div>
     <div class="info">
